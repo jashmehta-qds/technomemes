4bdc9553eb969182b576e79027a333372a915f6c
jash <jash@live.in>
2023-07-10 23:44:34 +0530

    updated about us
    Signed-off-by: jash <jash@live.in>

--- pages/about.tsx
+++ pages/about.tsx
@@ -11,6 +11,8 @@ const about = () => {
             <div className='about__content about__card p-4 rounded-lg w-4/5 md:w-4/12'>
                 <h1 className='heading text-xl font-bold text-white'>about technomemes</h1>
                 <p className='body-font mt-4 text-white'>A hall of memes articulating various moving parts in our tech revolution.</p>
+                <p className='body-font mt-4 text-white'>Each NFT in this ecosystem will be released under the CC0 license.</p>
+
                 <div>
                     <p className='mt-6 body-font'>made with ❤️ by anon_69</p>
                 </div>
